0ac1c23540ea190ada05b22a091cbbdf97a41738
NIKO <nikothewolfox@gmail.com>
2020-04-24 22:30:00 +0900

    StyleCopの警告を修正 region追加

--- AppWindowsForms/Views/MainForm.cs
+++ AppWindowsForms/Views/MainForm.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace FurAffinityClassifier.AppWindowsForms.Views
 {
+    /// <summary>
+    /// メイン画面
+    /// </summary>
     public partial class MainForm : Form
     {
+        #region Constructor
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
