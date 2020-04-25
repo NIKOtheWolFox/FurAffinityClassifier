9619f4cd7ac1467114429aa7108b93eee6c30273
NIKO <nikothewolfox@gmail.com>
2020-04-25 21:01:52 +0900

    IDが見つからなかったら次へ(起こらない想定)

--- CommonDotNetFramework/Models/ClassificationModel.cs
+++ CommonDotNetFramework/Models/ClassificationModel.cs
@@ -36,6 +36,7 @@ namespace FurAffinityClassifier.CommonDotNetFramework.Models
                     else
                     {
                         Console.WriteLine($"cannot find id from {f}");
+                        continue;
                     }
                 }
             }
