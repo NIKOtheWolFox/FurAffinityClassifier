3731f588c86d5f9a2f507c808329159715cc517c
NIKO <nikothewolfox@gmail.com>
2020-04-26 10:43:41 +0900

    コメントアウトしていたリファクタリング前の処理を削除

--- CommonDotNetFramework/Models/ClassificationModel.cs
+++ CommonDotNetFramework/Models/ClassificationModel.cs
@@ -83,67 +83,6 @@ namespace FurAffinityClassifier.CommonDotNetFramework.Models
                         file,
                         Path.Combine(settingData.ToFolder, folderName, Path.GetFileName(file)));
                 }
-
-                /*
-                foreach (var f in files2)
-                {
-                    var match = Regex.Match(f, @"[0-9]+\.(?<id>[a-z0-9-~^.]{3,}?)_.*");
-                    var id = string.Empty;
-                    if (match.Success)
-                    {
-                        id = match.Groups["id"].Value;
-                        ////Console.WriteLine($"id from {f} is {match.Groups["id"].Value}");
-                    }
-                    else
-                    {
-                        ////Console.WriteLine($"cannot find id from {f}");
-                        continue;
-                    }
-
-                    string foldername = id;
-                    if (settingData.IdFolderMappings.Exists(mapping => id == mapping.Id.Replace("_", string.Empty).ToLower()))
-                    {
-                        foldername = settingData.IdFolderMappings.Where(mapping => id == mapping.Id.Replace("_", string.Empty).ToLower()).FirstOrDefault().FolderName;
-                        ////Console.WriteLine($"ID({id}) is to {foldername}");
-                    }
-
-                    ////string folderpath = Path.Combine(settingData.ToFolder, foldername);
-                    ////Console.WriteLine($"path {folderpath} exist? : {Directory.Exists(folderpath)}");
-                    var foldersOnTo = Directory.GetDirectories(settingData.ToFolder);
-                    var folderTo = string.Empty;
-                    var matchedfolder = foldersOnTo.Where(ff => foldername.TrimEnd('.') == Path.GetFileName(ff).ToLower().Replace("_", string.Empty));
-                    if (matchedfolder.Count() > 1)
-                    {
-                        Console.WriteLine($"multiple fromders for {foldername}");
-                        continue;
-                    }
-                    else if (matchedfolder.Count() == 1)
-                    {
-                        folderTo = matchedfolder.First();
-                        Console.WriteLine($"folder for ID({id}) is to {folderTo}");
-                    }
-
-                    if (string.IsNullOrEmpty(folderTo))
-                    {
-                        if (settingData.CreateFolderIfNotExist)
-                        {
-                            Console.WriteLine($"I have to create filder {foldername}");
-                            Directory.CreateDirectory(Path.Combine(settingData.ToFolder, foldername.TrimEnd('.')));
-                            folderTo = Path.Combine(settingData.ToFolder, foldername.TrimEnd('.'));
-                        }
-                        else
-                        {
-                            Console.WriteLine("no folder, continue");
-                            continue;
-                        }
-                    }
-
-                    var filename = Path.GetFileName(f);
-                    File.Move(f, Path.Combine(folderTo, filename));
-
-                    Console.WriteLine($"DONE : {f} -> {Path.Combine(folderTo, filename)}");
-                }
-                */
             }
             catch (Exception e)
             {
