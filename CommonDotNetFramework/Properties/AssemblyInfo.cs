35cbc898cafe0d5f1d02188b594a906718bc6f90
NIKO <nikothewolfox@gmail.com>
2020-04-26 16:55:34 +0900

    実行ファイルのバージョンを0.1.0に変更 Commonの説明とCopyrightを追加

--- CommonDotNetFramework/Properties/AssemblyInfo.cs
+++ CommonDotNetFramework/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("CommonDotNetFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fur Affinity Classifier Common Module for .NET Framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CommonDotNetFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020 NIKO")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
