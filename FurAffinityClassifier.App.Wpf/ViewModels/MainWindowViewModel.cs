6f4537485d6695954a55c931d873682c0a1a7152
NIKO <nikothewolfox@gmail.com>
2020-04-30 14:26:45 +0900

    資料を参考にメモリリーク防止用の仕掛けを追加

--- FurAffinityClassifier.App.Wpf/ViewModels/MainWindowViewModel.cs
+++ FurAffinityClassifier.App.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 using FurAffinityClassifier.Common.Models;
@@ -11,17 +13,8 @@ namespace FurAffinityClassifier.App.Wpf.ViewModels
     /// <summary>
     /// メイン画面のViewModel
     /// </summary>
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged, IDisposable
     {
-        #region Private Field
-
-        /// <summary>
-        /// 設定機能
-        /// </summary>
-        private readonly SettingModel settingModel = new SettingModel();
-
-        #endregion
-
         #region Constructor
 
         /// <summary>
@@ -29,9 +22,19 @@ namespace FurAffinityClassifier.App.Wpf.ViewModels
         /// </summary>
         public MainWindowViewModel()
         {
-            settingModel.LoadFromFile();
+            SettingModel.LoadFromFile();
             Content = new ReactiveProperty<string>("TEST");
         }
+
+        #endregion
+
+        #region Public Event
+
+        /// <summary>
+        /// メモリリーク防止用のINotifyPropertyChangedの実装
+        /// </summary>
+        /// <inheritdoc cref="INotifyPropertyChanged.PropertyChanged"/>
+        public event PropertyChangedEventHandler PropertyChanged;
 
         #endregion
 
@@ -41,6 +44,33 @@ namespace FurAffinityClassifier.App.Wpf.ViewModels
         /// テスト用プロパティ
         /// </summary>
         public ReactiveProperty<string> Content { get; set; }
+
+        #endregion
+
+        #region Private Property
+
+        /// <summary>
+        /// 設定機能
+        /// </summary>
+        private SettingModel SettingModel { get; } = new SettingModel();
+
+        /// <summary>
+        /// 一括Disposeを行うためにReactiveXxをまとめるオブジェクト
+        /// </summary>
+        private CompositeDisposable Disposables { get; } = new CompositeDisposable();
+
+        #endregion
+
+        #region Public Method
+
+        /// <summary>
+        /// リソースを解放する
+        /// </summary>
+        /// <inheritdoc cref="IDisposable"/>
+        public void Dispose()
+        {
+            Disposables.Dispose();
+        }
 
         #endregion
     }
