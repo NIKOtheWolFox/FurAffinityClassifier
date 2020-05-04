f087770bd466b132eb5b2e6dfa014042cc1afd1b
NIKO <nikothewolfox@gmail.com>
2020-05-04 17:01:00 +0900

    .NET Coreのコンソールアプリケーションのプロジェクトを追加 StyleCopの警告を修正

---
+++ FurAffinityClassifier.App.Console/Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace FurAffinityClassifier.App.Console
+{
+    /// <summary>
+    /// アプリケーション
+    /// </summary>
+    public class Program
+    {
+        #region
+
+        /// <summary>
+        /// メインメソッド
+        /// </summary>
+        /// <param name="args">実行時の引数</param>
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+
+        #endregion
+    }
+}
