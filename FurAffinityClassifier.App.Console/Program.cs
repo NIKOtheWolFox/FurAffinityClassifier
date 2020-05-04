b8d89c5a7ec4b52f1f5032b49980549d7aa45011
NIKO <nikothewolfox@gmail.com>
2020-05-04 17:39:18 +0900

    実装したい処理をコメントで記載

--- FurAffinityClassifier.App.Console/Program.cs
+++ FurAffinityClassifier.App.Console/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using log4net;
 
 //// TODO : "CONSOLE"の名前検討
@@ -30,6 +31,23 @@ namespace FurAffinityClassifier.App.Console
         /// <param name="args">実行時の引数</param>
         public static void Main(string[] args)
         {
+            if (args.Count() == 0)
+            {
+                //// TODO : 分類処理の実装
+            }
+            else if (args.Count() == 1)
+            {
+                //// TODO : サブコマンドを見る(ヘルプ、設定の表示、バージョン)
+                ////        サブコマンドに従った表示をする
+                ////        更新後の保存が手動ならサブコマンドに「設定の保存」も追加
+            }
+            else
+            {
+                //// TODO : サブコマンドを見る(設定の変更)
+                ////        引数を見て更新する
+                ////        更新したあとの保存は自動でやる？
+            }
+
             CONSOLE.WriteLine("Hello World!");
             Logger.Debug("Hello World!!");
         }
