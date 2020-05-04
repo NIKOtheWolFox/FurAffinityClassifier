56666ab22116fa3da504910b111903e038cbcdbc
NIKO <nikothewolfox@gmail.com>
2020-05-04 17:44:16 +0900

    未実装であることを表示する

--- FurAffinityClassifier.App.Console/Program.cs
+++ FurAffinityClassifier.App.Console/Program.cs
@@ -34,22 +34,22 @@ namespace FurAffinityClassifier.App.Console
             if (args.Count() == 0)
             {
                 //// TODO : 分類処理の実装
+                CONSOLE.WriteLine("NOT IMPLEMENTED");
             }
             else if (args.Count() == 1)
             {
                 //// TODO : サブコマンドを見る(ヘルプ、設定の表示、バージョン)
                 ////        サブコマンドに従った表示をする
                 ////        更新後の保存が手動ならサブコマンドに「設定の保存」も追加
+                CONSOLE.WriteLine("NOT IMPLEMENTED");
             }
             else
             {
                 //// TODO : サブコマンドを見る(設定の変更)
                 ////        引数を見て更新する
                 ////        更新したあとの保存は自動でやる？
+                CONSOLE.WriteLine("NOT IMPLEMENTED");
             }
-
-            CONSOLE.WriteLine("Hello World!");
-            Logger.Debug("Hello World!!");
         }
 
         #endregion
