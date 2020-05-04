da8e088d7498a334ad8919c04b796493c66cfa03
NIKO <nikothewolfox@gmail.com>
2020-05-04 17:36:00 +0900

    コンソールアプリで使う定数のクラスを追加 サブコマンドを定数として定義

---
+++ FurAffinityClassifier.App.Console/Datas/ConsoleAppConst.cs
@@ -0,0 +1,20 @@
+namespace FurAffinityClassifier.App.Console.Datas
+{
+    internal class ConsoleAppConst
+    {
+        /// <summary>
+        /// サブコマンド ヘルプ
+        /// </summary>
+        public const string SubCommandHelp = "help";
+
+        /// <summary>
+        /// サブコマンド 設定の表示
+        /// </summary>
+        public const string SubCommandShowSetting = "show-setting";
+
+        /// <summary>
+        /// サブコマンド 設定の変更
+        /// </summary>
+        public const string SubCommandChangeSetting = "change-setting";
+    }
+}
