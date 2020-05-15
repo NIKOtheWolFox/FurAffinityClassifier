0126b5d8c7f2dcc2446b162cd38f8fd641a55480
NIKO <nikothewolfox@gmail.com>
2020-05-15 21:31:59 +0900

    WPFアプリがAppModelを使うように変更

--- FurAffinityClassifier.Common/Models/AppModel.cs
+++ FurAffinityClassifier.Common/Models/AppModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using FurAffinityClassifier.Common.Datas;
 
 namespace FurAffinityClassifier.Common.Models
 {
@@ -7,6 +8,55 @@ namespace FurAffinityClassifier.Common.Models
     /// </summary>
     public class AppModel
     {
+        #region Public Property
+
+        /// <summary>
+        /// 移動元フォルダー
+        /// </summary>
+        public string FromFolder
+        {
+            get => SettingModel.FromFolder;
+            set => SettingModel.FromFolder = value;
+        }
+
+        /// <summary>
+        /// 移動先フォルダー
+        /// </summary>
+        public string ToFolder
+        {
+            get => SettingModel.ToFolder;
+            set => SettingModel.ToFolder = value;
+        }
+
+        /// <summary>
+        /// 移動先のフォルダーが存在しないときに作成するか
+        /// </summary>
+        public bool CreateFolderIfNotExist
+        {
+            get => SettingModel.CreateFolderIfNotExist;
+            set => SettingModel.CreateFolderIfNotExist = value;
+        }
+
+        /// <summary>
+        /// 同名のファイルが存在するときに上書きするか
+        /// </summary>
+        public bool OverwriteIfExist
+        {
+            get => SettingModel.OverwriteIfExist;
+            set => SettingModel.OverwriteIfExist = value;
+        }
+
+        /// <summary>
+        /// IDと異なるフォルダーに分類する設定のリスト
+        /// </summary>
+        public List<ClassifyAsData> ClassifyAsDatas
+        {
+            get => SettingModel.ClassifyAsDatas;
+            set => SettingModel.ClassifyAsDatas = value;
+        }
+
+        #endregion
+
         #region Private Property
 
         /// <summary>
