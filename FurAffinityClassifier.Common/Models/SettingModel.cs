c4bec604fcf0ca009b1eb41bc0286d96152cba20
NIKO <nikothewolfox@gmail.com>
2020-05-16 20:28:13 +0900

    Json.NETをプロジェクトから削除

--- FurAffinityClassifier.Common/Models/SettingModel.cs
+++ FurAffinityClassifier.Common/Models/SettingModel.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using FurAffinityClassifier.Common.Datas;
 using Jil;
-using Newtonsoft.Json;
 using NLog;
 
 namespace FurAffinityClassifier.Common.Models
