9263d2c8a6fccb29c92f66defb56d0d1ed93c33c
NIKO <nikothewolfox@gmail.com>
2020-05-16 16:16:37 +0900

    不要なusingがあったので削除

--- FurAffinityClassifier.Common/Models/ClassificationModel.cs
+++ FurAffinityClassifier.Common/Models/ClassificationModel.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
