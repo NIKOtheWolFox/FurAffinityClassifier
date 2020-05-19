8ac9e9f35d46d4841e455575567ca4c71552d558
NIKO <nikothewolfox@gmail.com>
2020-05-19 10:11:30 +0900

    移動元の変更を実装

--- FurAffinityClassifier.App.Console/Models/ChangeSettingModel.cs
+++ FurAffinityClassifier.App.Console/Models/ChangeSettingModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using FurAffinityClassifier.App.Console.Properties;
 using FurAffinityClassifier.Common.Models;
 
 namespace FurAffinityClassifier.App.Console.Models
@@ -17,6 +19,9 @@ namespace FurAffinityClassifier.App.Console.Models
         /// </summary>
         private AppModel appModel;
 
+        /// <summary>
+        /// 実行時の引数
+        /// </summary>
         private string[] args;
 
         #endregion
@@ -44,15 +49,59 @@ namespace FurAffinityClassifier.App.Console.Models
         /// <returns>コンソールに出力する文字列</returns>
         public string Execute()
         {
-            switch (args.Length)
+            if (args.Length == 2)
             {
-                case 2:
-                case 3:
-                default:
-                    return "TEST";
+                var param = args[1].Split("=");
+                if (param.Length != 2)
+                {
+                    return Resources.InvalidOption;
+                }
+
+                var key = param[0];
+                var value = param[1];
+                switch (key)
+                {
+                    case "from-folder":
+                        if (Directory.Exists(value))
+                        {
+                            appModel.FromFolder = value;
+                            if (appModel.SaveSetting())
+                            {
+                                return "Setting changed successfully";
+                            }
+                            else
+                            {
+                                return "Failed to change setting";
+                            }
+                        }
+                        else
+                        {
+                            return "Given folder not exist";
+                        }
+
+                    case "to-folder":
+                        return "to-folder";
+                    case "create-folder":
+                        return "create-folder";
+                    case "overwrite":
+                        return "overwrite";
+                    default:
+                        return Resources.InvalidOption;
+                }
+            }
+            else if (args.Length == 3)
+            {
+                return "MULTI";
+            }
+            else
+            {
+                return Resources.InvalidOption;
             }
         }
 
+        #endregion
+
+        #region Private Method
         #endregion
     }
 }
