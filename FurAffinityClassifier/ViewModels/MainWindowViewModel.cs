f401252a69bab474095bbb833238f26e20a2743d
NIKO <nikothewolfox@gmail.com>
2021-02-14 10:30:39 +0900

    ViewModelの親クラスを設定して使用するModelを追加

--- FurAffinityClassifier/ViewModels/MainWindowViewModel.cs
+++ FurAffinityClassifier/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,52 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FurAffinityClassifier.Models;
+using NLog;
+using Prism.Mvvm;
+using Reactive.Bindings;
 
 namespace FurAffinityClassifier.ViewModels
 {
     /// <summary>
     /// メイン画面のViewModel
     /// </summary>
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
+        #region Private Field
+
+        /// <summary>
+        /// NLogのロガー
+        /// </summary>
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            Debug.WriteLine("test");
+        }
+
+        #endregion
+
+        #region Public Property
+        #endregion
+
+        #region Private Property
+
+        /// <summary>
+        /// アプリケーションの機能
+        /// </summary>
+        private AppModel AppModel { get; } = new AppModel();
+
+        #endregion
     }
 }
