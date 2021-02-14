b624fd590feedf6820f57fede674f59c34d2f887
NIKO <nikothewolfox@gmail.com>
2021-02-14 10:23:38 +0900

    ModelとViewModelのクラスだけ作成

---
+++ FurAffinityClassifier/Models/SettingModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurAffinityClassifier.Models
+{
+    /// <summary>
+    /// 設定機能
+    /// </summary>
+    public class SettingModel
+    {
+    }
+}
---
+++ FurAffinityClassifier/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurAffinityClassifier.ViewModels
+{
+    /// <summary>
+    /// メイン画面のViewModel
+    /// </summary>
+    public class MainWindowViewModel
+    {
+    }
+}
