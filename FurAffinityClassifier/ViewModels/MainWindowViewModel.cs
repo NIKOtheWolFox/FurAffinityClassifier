ed02a210309b4ede2ea738306c6c34ed256450ae
NIKO <nikothewolfox@gmail.com>
2021-02-14 11:29:38 +0900

    ViewModelの最初で設定を読み込む処理を本来のものに戻す

--- FurAffinityClassifier/ViewModels/MainWindowViewModel.cs
+++ FurAffinityClassifier/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 using FurAffinityClassifier.Models;
 using NLog;
 using Prism.Mvvm;
+using System.Diagnostics;
 
 namespace FurAffinityClassifier.ViewModels
 {
@@ -25,9 +26,9 @@ namespace FurAffinityClassifier.ViewModels
         /// </summary>
         public MainWindowViewModel()
         {
-            //Debug.WriteLine(AppModel.LoadSetting());
-            //Debug.WriteLine(AppModel.SaveSetting());
-            ////AppModel.LoadSetting();
+            ////Debug.WriteLine(AppModel.LoadSetting());
+            ////Debug.WriteLine(AppModel.SaveSetting());
+            AppModel.LoadSetting();
         }
 
         #endregion
