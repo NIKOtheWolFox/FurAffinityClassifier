0fd3f4158a09cf22344aa0d8f922dec66fbdb393
NIKO <nikothewolfox@gmail.com>
2021-04-30 10:47:41 +0900

    設定と分類のModelを追加

---
+++ FurAffinityClassifier/Models/IClassificationModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurAffinityClassifier.Models
+{
+    /// <summary>
+    /// 分類機能 interface
+    /// </summary>
+    public interface IClassificationModel
+    {
+    }
+}
