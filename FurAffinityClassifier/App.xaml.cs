7db71c64bd598e9ec1027b31d80741ea73741b84
NIKO <nikothewolfox@gmail.com>
2022-02-20 10:36:55 +0900

    不要なusingとコメントアウトした旧処理を削除

--- FurAffinityClassifier/App.xaml.cs
+++ FurAffinityClassifier/App.xaml.cs
@@ -1,7 +1,4 @@
 using System.Windows;
-using FurAffinityClassifier.Helpers;
-using FurAffinityClassifier.Models;
-using FurAffinityClassifier.Views;
 
 namespace FurAffinityClassifier
 {
@@ -10,28 +7,5 @@ namespace FurAffinityClassifier
     /// </summary>
     public partial class App : Application
     {
-        /*
-        /// <summary>
-        /// <see cref="PrismApplicationBase.CreateShell()"/>
-        /// </summary>
-        /// <returns>メイン画面</returns>
-        protected override Window CreateShell()
-        {
-            return Container.Resolve<MainWindow>();
-        }
-
-        /// <summary>
-        /// <see cref="PrismApplicationBase.RegisterTypes(IContainerRegistry)"/>
-        /// </summary>
-        /// <param name="containerRegistry">Container by Prism</param>
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            containerRegistry.Register<IAppModel, AppModel>();
-            containerRegistry.Register<ISettingsModel, SettingsModel>();
-            containerRegistry.Register<IClassificationModel, ClassificationModel>();
-
-            containerRegistry.Register<IDialogHelper, DialogHelper>();
-        }
-        */
     }
 }
--- FurAffinityClassifier/ViewModels/ViewModelLocator.cs
+++ FurAffinityClassifier/ViewModels/ViewModelLocator.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FurAffinityClassifier.Helpers;
 using FurAffinityClassifier.Models;
 using Microsoft.Extensions.DependencyInjection;
