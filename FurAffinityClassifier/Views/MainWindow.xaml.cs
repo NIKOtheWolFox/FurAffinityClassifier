a853ebe5536853351de01074131055242c68bc2a
NIKO <nikothewolfox@gmail.com>
2022-03-06 16:44:42 +0900

    XAMLで定義したリソースをコードで使うテスト

--- FurAffinityClassifier/Views/MainWindow.xaml.cs
+++ FurAffinityClassifier/Views/MainWindow.xaml.cs
@@ -22,6 +22,22 @@ namespace FurAffinityClassifier.Views
             {
                 System.Diagnostics.Debug.WriteLine("DataContext != null");
             }
+
+            try
+            {
+                if (App.Current.Resources["Locator"] is FurAffinityClassifier.Locator X)
+                {
+                    System.Diagnostics.Debug.WriteLine("App.Current.Resources[Locator] as FurAffinityClassifier.Locator != null");
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("App.Current.Resources[Locator] as FurAffinityClassifier.Locator == null");
+                }
+            }
+            catch
+            {
+                // do nothing
+            }
         }
     }
 }
