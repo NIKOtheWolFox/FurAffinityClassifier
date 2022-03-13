85e627757a52bb62d057e9765a4a40ea225f7503
NIKO <nikothewolfox@gmail.com>
2022-03-13 09:42:16 +0900

    メイン画面のテスト用コードを削除

--- FurAffinityClassifier/Views/MainWindow.xaml.cs
+++ FurAffinityClassifier/Views/MainWindow.xaml.cs
@@ -13,31 +13,6 @@ namespace FurAffinityClassifier.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            if (DataContext == null)
-            {
-                System.Diagnostics.Debug.WriteLine("DataContext == null");
-            }
-            else
-            {
-                System.Diagnostics.Debug.WriteLine("DataContext != null");
-            }
-
-            try
-            {
-                if (App.Current.Resources["Locator"] is FurAffinityClassifier.Locator X)
-                {
-                    System.Diagnostics.Debug.WriteLine("App.Current.Resources[Locator] as FurAffinityClassifier.Locator != null");
-                }
-                else
-                {
-                    System.Diagnostics.Debug.WriteLine("App.Current.Resources[Locator] as FurAffinityClassifier.Locator == null");
-                }
-            }
-            catch
-            {
-                // do nothing
-            }
         }
     }
 }
